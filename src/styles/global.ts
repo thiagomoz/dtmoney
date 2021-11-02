138e2012462b6079ba8914d80e10b1a820ad22c0
Thiago Oliveira <thiagomo@me.com>
2021-11-02 13:06:36 -0300

    Criando estilos glabais

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF;
+  }
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  // font-size: 16px (Desktop Base)
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 85.5%; // 14px
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disable] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
