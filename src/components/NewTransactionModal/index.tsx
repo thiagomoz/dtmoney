ea0bb008ec194fdcfabdc724da92024cb375606c
Thiago Oliveira <thiagomo@me.com>
2021-11-03 19:24:05 -0300

    Create Transaction Modal form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './style';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -9,8 +10,33 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   return (
     <Modal 
       isOpen={isOpen} 
-      onRequestClose={onRequestClose}>
-        <h2>Cadastrar Transação</h2>
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+      >
+
+        <Container>
+          <h2>Cadastrar Transação</h2>
+
+
+          <input 
+          placeholder="Título"
+          />
+
+          <input 
+          type="number"
+          placeholder="Valor"
+          />
+
+          <input 
+          placeholder="Categoria"
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+          
+        </Container>
       </Modal>
   );
 }
--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
-export const Cointer = styled.div`
+export const Container = styled.form`
   
 `;
