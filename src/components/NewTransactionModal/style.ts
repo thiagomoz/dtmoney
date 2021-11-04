190f4ac0692b5b021870d1591be7934222e46c1d
Thiago Oliveira <thiagomo@me.com>
2021-11-04 14:38:30 -0300

    Add Transaction Button Type Props

--- src/components/NewTransactionModal/style.ts
+++ src/components/NewTransactionModal/style.ts
@@ -55,12 +55,19 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
 
-  button {
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: transparent;
+    background: ${(props) => props.isActive ? '#eee' : 'transparent'};
+
     display: flex;
     align-items: center;
     justify-content: center;
@@ -82,5 +89,4 @@ export const TransactionTypeContainer = styled.div`
       font-size: 1rem;
       color: var(--text-title);
     }
-  }
 `;
